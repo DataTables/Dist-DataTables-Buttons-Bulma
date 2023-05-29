db67425445b821214b0a789e05280a19112713ec
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-05-29 15:26:45 +0100

    582751d57e927f08a894058518561bf0f9adba97 New: Dark mode support for DataTables styling
    Sync to source repo @582751d57e927f08a894058518561bf0f9adba97

--- js/buttons.bulma.mjs
+++ js/buttons.bulma.mjs
@@ -2,9 +2,12 @@
  * ©2021 SpryMedia Ltd - datatables.net/license
  */
 
-import $ from 'jquery';
+import jQuery from 'jquery';
 import DataTable from 'datatables.net-bm';
 import Buttons from 'datatables.net-buttons';
+
+// Allow reassignment of the $ variable
+let $ = jQuery;
 
 
 $.extend( true, DataTable.Buttons.defaults, {
